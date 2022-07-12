5ce8bc830bb5604aef8f8b6ba6878f721657a86b
HugoHerreraH <launchx04415@innovaccion.mx>
2022-07-11 22:25:08 -0500

    Proyecto creado

---
+++ test/models/User.test.js
@@ -0,0 +1,16 @@
+const User = require('../../app/models/user')
+
+describe("Unit Tests for User class", () => {
+
+    test('Create an User object', () => {
+
+        const user = new User(1, "HugoH", "Hugo", "Bio", "dateCreated", "lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("HugoH")
+        expect(user.name).toBe("Hugo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    })
+})
